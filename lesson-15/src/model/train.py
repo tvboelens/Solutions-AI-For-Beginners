cf59b017c1af4eb213fab16ec12d7cc1578b5ce3
tvboelens <tvboelens@gmail.com>
2024-01-23 15:00:58 +0100

    Fixed bug in build_vocab and completed dataset creation

--- lesson-15/src/model/train.py
+++ lesson-15/src/model/train.py
@@ -1,12 +1,17 @@
 import ssl
 import os
 import collections
-#import torch
+import random
+import torch
+from torch.utils.data import random_split, DataLoader, Dataset,IterableDataset
+from tqdm import tqdm
 import nltk
 from torchtext.data.utils import get_tokenizer
 from torchtext import vocab as V
 import yaml
 import ssl
+
+import time
 
 
 try:
@@ -20,6 +25,37 @@ from nltk import tokenize
 
 CONFIG_PATH = 'src/config/'
 
+
+class SimpleIterableDataset(IterableDataset):
+    def __init__(self, X, Y):
+        super().__init__()
+        self.data = []
+        for i in tqdm(range(len(X)), desc="Building dataset..."):
+            self.data.append((X[i], Y[i]))
+        random.shuffle(self.data)
+
+    def __iter__(self):
+        return iter(self.data)
+    
+    def __len__(self):
+        return len(self.data)
+
+
+class EmbeddingDataset(Dataset):
+    def __init__(self, X, Y):
+        super().__init__()
+        self.data = []
+        for i in tqdm(range(len(X)), desc="Building dataset..."):
+            self.data.append((Y[i], X[i]))
+        random.shuffle(self.data)
+
+    def __getitem__(self,idx):
+        return self.data[idx]
+
+
+    def __len__(self):
+        return len(self.data)
+
 def load_config(config_name):
     with open(os.path.join(CONFIG_PATH, config_name)) as file:
         config = yaml.safe_load(file)
@@ -31,8 +67,9 @@ def build_vocab(text, min_freq=1, vocab_size=5000):
     tokens = tokenize.word_tokenize(text)
     cleaned_tokens = [token.lower() for token in tokens if token.isalpha()]
     counter = collections.Counter(cleaned_tokens)
-    print(counter.most_common(vocab_size)[:100])
-    vocab = V.vocab(collections.Counter(dict(counter.most_common(vocab_size))), min_freq=min_freq)
+    vocab = V.vocab(collections.Counter(dict(counter.most_common(vocab_size))), min_freq=min_freq, specials=["<unk>"])
+    vocab.set_default_index(vocab['<unk>'])
+    print('Vocab complete')
     return vocab
 
 
@@ -45,8 +82,25 @@ def to_skip_gram(sent, window_size=2):
     return res
 
 
-def encode(x, vocabulary, tokenizer=tokenize.word_tokenize()):
-    return [vocabulary[s.lower()] for s in tokenizer(x)]
+def encode(sent, vocab):
+    encoded_token = []
+    for token in tokenize.word_tokenize(sent):
+        encoded_token.append(vocab[token.lower()])
+    return encoded_token
+
+def get_dataset(text, ds_len, vocab, window_size):
+    X = []
+    Y = []
+    for _, sent in zip(range(ds_len), text):
+        for w1, w2 in to_skip_gram(encode(sent, vocab), window_size):
+            X.append(w1)
+            Y.append(w2)
+
+    X = torch.tensor(X)
+    Y = torch.tensor(Y)
+    ds = SimpleIterableDataset(X,Y)
+    return ds
+
 
 
 
@@ -54,8 +108,16 @@ def main(config):
     text = open('data/shakespeare.txt', 'rb').read().decode(encoding='utf-8').replace('\n',' ')
     vocab = build_vocab(text)
     text = tokenize.sent_tokenize(text)
-    #TODO: Split text into train and test part
-    #Then make torch dataset out of it
+    ds = get_dataset(text, ds_len=config["ds_len"],vocab=vocab, window_size=config["window_size"])
+    starttime = time.time()
+    print(f"Splitting dataset and creating dataloaders")
+    train_set, test_set = random_split(
+        ds, [config["ds_split"], 1-config["ds_split"]])
+    train_loader = DataLoader(train_set, batch_size=config["bs"])
+    test_loader = DataLoader(test_set, batch_size=config["bs"])
+    print(f"Complete, time={round(time.time()-starttime,2)} seconds")
+
+    
 
 
 
