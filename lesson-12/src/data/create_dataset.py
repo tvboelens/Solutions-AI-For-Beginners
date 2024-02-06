fba2bcdb4f3a5377d6a8e29bae4a173df1a91a93
tvboelens <tvboelens@gmail.com>
2024-02-06 14:22:16 +0100

    Added messages

--- lesson-12/src/data/create_dataset.py
+++ lesson-12/src/data/create_dataset.py
@@ -21,17 +21,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if not os.path.exists(dataset_path):
         if args.gcp_bucket is not None:
+            print("Fetching data from Google Cloud Storage...")
             storage_client = storage.Client()
             bucket = storage_client.bucket(
                 args.gcp_bucket)
             blob = bucket.blob('mads_ds_1192.zip')
             blob.download_to_filename('mads_ds_1192.zip')
-            
+        
+        print("Unzipping dataset...")    
         with zipfile.ZipFile('mads_ds_1192.zip') as file:
             file.extractall()
         for dir in os.listdir(os.path.join(dataset_path, dataset_path)):
             shutil.move(os.path.join(dataset_path, dataset_path, dir),
                     os.path.join(dataset_path, dir))
         shutil.rmtree(os.path.join(dataset_path, dataset_path))
+        print("Dataset created.")
             
 
